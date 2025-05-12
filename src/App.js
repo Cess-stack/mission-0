a36b7bcddc5b25eff6589fa8d071f2317722d24c
Cess-stack <celesset@missionreadyhq.com>
2025-05-12 13:48:23 +1200

    added notes to help me with my presentation to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// This is the main app component. It serves as the root of the React component tree
+// and is responsible for rendering the primary page of the application,
+// which in this case is the NZMarketingAssociationPage.
+
 import React from "react";
 import NZMarketingAssociationPage from "./NZMarketingAssociationPage";
 
